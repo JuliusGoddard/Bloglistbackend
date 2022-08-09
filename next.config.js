f715f70090df910710c34b5fad559b922a25fd51
Julius Goddard <julius.goddard@webforprofessionals.com>
2022-08-09 13:53:26 +0100

    efefefef

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.resolve.alias.https = "https-browserify";
-      config.resolve.alias.http = "http-browserify";
-      return config;
-    },
-  };
